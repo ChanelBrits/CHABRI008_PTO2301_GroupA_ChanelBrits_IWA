c393d8828ae4f20004124a764c94a1a5bec8d7e5
ChanelBrits <126657808+ChanelBrits@users.noreply.github.com>
2023-07-17 10:04:43 +0200

    Fixed syntax errors in comments and log

---
+++ IWA-02/Challenge1/js/scripts.js
@@ -0,0 +1,7 @@
+/* This is the first name of the user */
+const user = 'John';
+
+/* This is the last name of the user */
+const surname = 'Smith';
+
+console.log(`User: ${user} ${surname}`) // User: John Smith
