fb08569e92c510687e615338c0a38a28f04e8d96
ChanelBrits <126657808+ChanelBrits@users.noreply.github.com>
2023-07-24 10:07:27 +0200

    Fixed syntax error

--- IWA-03/Challenge2/js/scripts.js
+++ IWA-03/Challenge2/js/scripts.js
@@ -1,5 +1,5 @@
-import {role} from './nwabisa.js';
-import {role} from './johannes.js';
-import {role} from './alex.js';
+import { role } from './nwabisa.js';
+import { role } from './johannes.js';
+import { role } from './alex.js';
 
-console.log(`Roles: ${nwabisa.role}, ${johanes.role}, ${alex.role}`)
+console.log(`Roles: ${nwabisa.role}, ${johannes.role}, ${alex.role}`)
