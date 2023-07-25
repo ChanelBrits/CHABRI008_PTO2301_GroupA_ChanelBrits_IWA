bca00a26a9aca0f002a122bf730a9d6104364481
ChanelBrits <126657808+ChanelBrits@users.noreply.github.com>
2023-07-25 10:35:19 +0200

    Fixed syntax errors with template literals

--- IWA-07/Challenge1/js/scripts.js
+++ IWA-07/Challenge1/js/scripts.js
@@ -1,2 +1,6 @@
 const value = "3"
 console.log(`The value is ${parseInt(value) + 4 + parseInt(value)}`)
+
+/* The original console.log statement concatenated the values together, which
+ * resulted in 343 instead of 10. The reason for this is because value is a string.
+ */
---
+++ IWA-07/Challenge2/js/scripts.js
@@ -0,0 +1,4 @@
+const nickname= null; //"Timmy";
+const firstName = "Timothy";
+
+console.log(`Good Morning, ${nickname || firstName}!`)
