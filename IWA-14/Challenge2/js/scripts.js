b26e3268930af25b230de25454fce20ac34a8c7a
ChanelBrits <126657808+ChanelBrits@users.noreply.github.com>
2023-07-27 16:15:51 +0200

    Fixed syntax with functions and this keyword

---
+++ IWA-14/Challenge2/js/scripts.js
@@ -0,0 +1,35 @@
+const add = (a, b) => a + b;
+
+const multiply = (a, b) => a * b;
+
+const internal = function() {
+    console.log(this.add(this.internal.a, this.internal.b) * this.internal.c);
+    return this;
+};
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
